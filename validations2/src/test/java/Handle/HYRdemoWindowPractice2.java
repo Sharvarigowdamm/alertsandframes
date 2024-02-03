654dc9e6b300f5a5893d09e2069d40385973e10e
Sharvarigowdamm <158027023+Sharvarigowdamm@users.noreply.github.com>
2024-02-03 09:58:46 +0000

    Add files via upload

--- validations2/src/test/java/Handle/HYRdemoWindowPractice2.java
+++ validations2/src/test/java/Handle/HYRdemoWindowPractice2.java
@@ -1,4 +1,5 @@
 package Handle;
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Set;
@@ -10,7 +11,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HYRdemoWindowPractice2 {
 
 	public static void main(String[] args) {
@@ -18,31 +21,34 @@ public class HYRdemoWindowPractice2 {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
 		driver.manage().window().maximize();
-		String parent=driver.getWindowHandle();
-		driver.findElement(By.id("newWindowBtn")).click();
+		String parent = driver.getWindowHandle();
+		driver.findElement(By.id("newTabBtn")).click();
 		Set<String> childs = driver.getWindowHandles();
 		System.out.println(parent);
-		for(String child:childs) {
-			if(!child.equals(parent)) {
-				Alert alert=driver.switchTo().alert();
+		for (String child : childs) {
+			if (!child.equals(parent)) {
+				/* Alert alert=driver.switchTo().alert(); */
 				driver.switchTo().window(child);
 				JavascriptExecutor js = (JavascriptExecutor) driver;
-				js.executeScript("window.scrollBy(0,350)", "");
-				driver.findElement(By.id("alertBox")).click();
-				System.out.println(alert.getText());
+				js.executeScript("window.scrollBy(0,450)", "");
+				driver.findElement(By.xpath("//*[@id=\"alertBox\"]")).click();
+				Alert alert = driver.switchTo().alert();
 				alert.accept();
+
 				driver.findElement(By.id("confirmBox")).click();
-				driver.findElement(By.id("alertBox")).click();
 				System.out.println(alert.getText());
 				alert.accept();
-				
+
 				driver.findElement(By.id("promptBox")).click();
-				WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
-				Alert alert2=wait.until(ExpectedConditions.alertIsPresent());
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+				Alert alert2 = wait.until(ExpectedConditions.alertIsPresent());
 				alert2.sendKeys("Sharvari");
 				alert2.accept();
+				System.out.println("everything done!!");
+				driver.quit();
+
 			}
-			
+
 		}
 
 	}
