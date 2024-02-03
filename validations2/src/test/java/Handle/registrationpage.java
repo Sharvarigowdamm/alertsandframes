6fdb0a9878cf8ea3ffedc32f5c17d60f0a4b948b
Sharvarigowdamm <158027023+Sharvarigowdamm@users.noreply.github.com>
2024-02-03 09:56:52 +0000

    Add files via upload

--- validations2/src/test/java/Handle/registrationpage.java
+++ validations2/src/test/java/Handle/registrationpage.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class registrationpage {
 
@@ -28,18 +30,24 @@ public class registrationpage {
          
 		JavascriptExecutor js1 = (JavascriptExecutor) driver;
 		js1.executeScript("window.scrollBy(0,350)", "");
+		 WebElement optionslist=driver.findElement(By.id("Skills"));
+		Select dd=new Select(optionslist);
+		dd.selectByIndex(1);
 		
 		driver.findElement(By.xpath("//*[@id=\"countries\"]")).click();
 		
 		driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[10]/div/span/span[1]/span")).click();
 	
-		driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("aus");
+		WebElement autoselect=driver.findElement(By.xpath("/html/body/span/span/span[1]/input"));
+		autoselect.sendKeys("aus");
+		autoselect.sendKeys(Keys.ENTER);
 		
-		  WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
-		  WebElement
-		  options=wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(
-		  "select2-country-container")));
-		  options.click();
+		/*
+		 * WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
+		 * WebElement
+		 * options=wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(
+		 * "select2-country-container"))); options.click();
+		 */
 		 
 		
 		//driver.findElement(By.id("select2-country-container")).click();
